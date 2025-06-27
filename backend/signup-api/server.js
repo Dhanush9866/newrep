629e80dbb8489818c8044cdeee0e8215c5754d37
Sunand-Vemavarapu <sunandvemavarapu@gmail.com>
2025-06-27 21:55:48 +0530

    Update server.js

--- backend/signup-api/server.js
+++ backend/signup-api/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
-const BDurl = "mongodb://127.0.0.1:27017/signupDB";
+const BDurl = MONGODBURL;
 
 dotenv.config();
 const app = express();
